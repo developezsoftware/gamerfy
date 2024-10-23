d3d9702ae41778fc28b7885f7277044baa2494ef
Developez Software <88462760+developezsoftware@users.noreply.github.com>
2024-10-23 14:30:12 -0400

    use react query for fetching genres

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "c7b18323a47d40c394ea5b019646b1f5",
+    key: "48a5ff86c03b454d80eb861af324f543",
   },
 });
